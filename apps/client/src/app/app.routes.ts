b9591254b18cc45d251c4b595f1c691056203380
Daniel Cifuentes <daniele.cifuentesb@uqvirtual.edu.co>
2026-01-13 19:44:30 -0500

    Delete double dir. checkpoint before hx

--- apps/client/src/app/app.routes.ts
+++ apps/client/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './pages/home/home';
 import { Login } from './pages/auth/login/login';
 import { Register } from './pages/auth/register/register';
 import { Cart } from './pages/cart/cart';
+import { Checkout } from './pages/checkout/checkout';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
   { path: 'cart', component: Cart },
+  { path: 'checkout', component: Checkout },
   { path: '**', redirectTo: '' }
 ];
